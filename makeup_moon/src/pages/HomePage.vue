c5f2b17690441ae9ba5c5aeb42cab6f71099583e
rinnmoon <kojurinnmymelo@gmail.com>
2022-06-22 23:31:44 +0900

    ルーター設定の確認
    ルーターがちゃんと設定されているかの確認をしてほしい。

---
+++ makeup_moon/src/pages/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+<router-view />
+
+<div>moon</div>
+
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script lang="ts">
+import{ defineComponent } from "vue"
+
+
+export default defineComponent({
+name:"HOmePage",
+
+})
+
+</script>
